8bc1d0ec7f3d68d27df52955b21764ded17e7f15
sarapapa-sp <shubhampawar1705@gmail.com>
2022-09-09 18:43:36 +0530

    completed 1.0v of Tic Tac Toe

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -5,9 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    boolean gameActive = true;
     /*
     * Player 0 - X
     * Player 1 - 0
@@ -32,18 +33,86 @@ public class MainActivity extends AppCompatActivity {
 //        Getting the image tap
         ImageView img =(ImageView) view;
         int tappedImg = Integer.parseInt(img.getTag().toString());
+        TextView status = findViewById(R.id.status);
+        if(!gameActive){
+            gameReset(view);
+        }
         if(gameStatus[tappedImg] == 2){
             gameStatus[tappedImg]=activePlayer;
             img.setTranslationY(-1000f);
             if(activePlayer == 0){
                 img.setImageResource(R.drawable.xtic);
                 activePlayer=1;
+                status.setText("0's Turn , Tap to play");
             }else{
                 img.setImageResource(R.drawable.o);
                 activePlayer=0;
+                status.setText("X's Turn , Tap to play");
             }
             img.animate().translationYBy(1000f).setDuration(300);
         }
+
+        // check if any player has won or not
+
+        for(int[] winPosition : winningPositions){
+            if(
+                    gameStatus[winPosition[0]] == gameStatus[winPosition[1]] &&
+                    gameStatus[winPosition[2]] == gameStatus[winPosition[1]] &&
+                    gameStatus[winPosition[0]] != 2
+            ){
+//                Somebody has won !!
+                String winnerString;
+                    if(gameStatus[winPosition[0]]==0){
+//                        X has won
+                        winnerString="X has Won";
+                    }else{
+//                        0 has won
+                        winnerString="0 has Won";
+                    }
+                    status.setText(winnerString);
+
+                    gameActive=false;
+
+
+            }
+        }
+
+        int flag= 0;
+        for(int val : gameStatus){
+            if(val == 2){
+                flag=1;
+            }
+        }
+//        if flag remains 0 means all the places are full
+//        if flags becomes 1 means atleast one place is empty
+        if(flag == 0){
+            gameReset(view);
+        }
+
+
+    }
+
+//    Resting the Game values
+    public void gameReset(View view){
+        gameActive=true;
+        activePlayer=0;
+
+        for(int i=0;i<gameStatus.length;i++){
+            gameStatus[i]=2;
+        }
+
+        ((ImageView) findViewById(R.id.imageView0)).setImageResource(0);
+        ((ImageView) findViewById(R.id.imageView1)).setImageResource(0);
+        ((ImageView) findViewById(R.id.imageView2)).setImageResource(0);
+        ((ImageView) findViewById(R.id.imageView3)).setImageResource(0);
+        ((ImageView) findViewById(R.id.imageView4)).setImageResource(0);
+        ((ImageView) findViewById(R.id.imageView5)).setImageResource(0);
+        ((ImageView) findViewById(R.id.imageView6)).setImageResource(0);
+        ((ImageView) findViewById(R.id.imageView7)).setImageResource(0);
+        ((ImageView) findViewById(R.id.imageView8)).setImageResource(0);
+
+        TextView status = findViewById(R.id.status);
+        status.setText("X's Turn , Tap to play");
     }
 
     @Override
